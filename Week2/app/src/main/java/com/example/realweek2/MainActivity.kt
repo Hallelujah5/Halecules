d9f2d4a576fa832f72a1aa6ebec0f8b01b599d1f
Hallelujah5 <tdmhale5@gmail.com>
2025-01-06 16:25:10 +0700

    week2

---
+++ Week2/app/src/main/java/com/example/realweek2/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.realweek2
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+
+lateinit var btnPlus: Button
+lateinit var btnMinus: Button
+lateinit var txtName:EditText
+lateinit var txtName2:EditText
+lateinit var txtResult:EditText
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    btnPlus= findViewById(R.id.btnPlus)
+    btnMinus= findViewById(R.id.btnMinus)
+    txtName= findViewById(R.id.txtName)
+    txtName2= findViewById(R.id.txtName2)
+    txtResult= findViewById(R.id.txtResult)
+
+    btnPlus.setOnClickListener(this)
+    btnMinus.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v: View?) {
+        var Num1 = txtName.text.toString().toDouble()
+        var Num2 = txtName2.text.toString().toDouble()
+        when(v?.id){
+            R.id.btnPlus->{
+                var Result = Num1 + Num2
+                txtResult.setText(Result.toString())
+            }
+            R.id.btnMinus->{
+                var Result = Num1 - Num2
+                txtResult.setText(Result.toString())
+            }
+
+        }
+    }
+}
